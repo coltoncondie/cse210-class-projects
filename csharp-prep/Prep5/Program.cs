5f2d2f0dabf1a8c57b302980c5237fcf3484dbce
coltoncondie <coltoncondie@outlook.com>
2024-11-09 23:56:39 -0700

    Making functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+        
+        
+        {
+            DisplayMessage();
+            string username = GetUserName();
+            int favoriteNumber = GetFavoriteNumber();
+            int squaredNumber = SquareNumber(favoriteNumber);
+            DisplayResult(username, squaredNumber);
+        }
+        
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string GetUserName()
+        {
+            Console.Write("Please enter your Username here: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+        static int GetFavoriteNumber()
+        {
+            Console.Write("Please enter your favorite number here: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            return favoriteNumber;
+        }
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+        static void DisplayResult(string username, int squaredNumber)
+        {
+            Console.WriteLine($"{username}, your favorite number squared is: {squaredNumber}");
+        }
+        
+
     }
 }
