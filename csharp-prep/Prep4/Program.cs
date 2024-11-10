1c74321683ffc8d79ab2845724bf8c8b52d12a99
coltoncondie <coltoncondie@outlook.com>
2024-11-09 22:04:33 -0700

    Created a list of numbers that collects the sum, average, and biggest number.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Hello user, Please enter a list of numbers, and when you are done press 0. ");
+
+        List<int> numbers = new List<int>();
+        int newNumber;
+
+        do
+        {
+            Console.WriteLine("What number would you like to add to the list?");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+                Console.WriteLine($"Number {newNumber} Successfully Added to list. ");
+            }
+
+        } while (newNumber != 0);
+
+        Console.WriteLine("This is the List of Numbers: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        int sum = numbers.Sum();
+        Console.WriteLine($"The total Sum for all numbers is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average for the list is: {average}");
+
+        int biggestNumber = numbers.Max();
+        Console.WriteLine($"The Biggest number in the list is: {biggestNumber}");
+
     }
+
 }
