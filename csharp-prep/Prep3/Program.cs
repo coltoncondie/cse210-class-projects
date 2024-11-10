24fa912b1063d2920607db69e41d517447043ad5
coltoncondie <coltoncondie@outlook.com>
2024-11-09 17:35:10 -0700

    I created a loop game I will need to improve.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGen = new Random();
+        int magicNum = randomGen.Next(1, 101);
+        
+        int guessNum = -1;
+        Console.WriteLine("Welcome Player, This is Magic NuMbEr! The name of the game is to guess the Auto generated number. Good Luck! ");
+
+        while (guessNum != magicNum)
+        {    
+            Console.WriteLine("What is your guess? ");
+            guessNum = int.Parse(Console.ReadLine());
+            if (magicNum > guessNum)
+            {
+                Console.WriteLine("Bigger");
+            }
+            else if (magicNum < guessNum)
+            {
+                Console.WriteLine("smaller");
+            }
+            else
+            {
+                Console.WriteLine("Congrats That is the Magic number!");
+            }
+        }       
     }
 }
