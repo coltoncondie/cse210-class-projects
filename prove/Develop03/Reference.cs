a8e7d9646377f454daa4d2e0d059e689ad425f78
coltoncondie <coltoncondie@outlook.com>
2024-11-24 00:32:05 -0700

    Had a Syntax Error due to null I had to assign it an interesting value of (int?). That was in the Reference Class Line 9.

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -6,7 +6,7 @@ public class Reference
     private string _book;
     private int _chapter;
     private int _startVerse;
-    private int _endVerse; // Nullalble for single-verse references
+    private int? _endVerse; // Nullalble for single-verse references
 
     // Constructor for a single verse
     public Reference(string book, int chapter, int verse)
